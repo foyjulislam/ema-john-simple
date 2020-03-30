ee84486961855fbad1c689e8d241028b170d94f9
foyjulislam <foyjulislam.syl20@gmail.com>
2020-03-30 18:42:22 +0600

    shipment completed

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
                 <Route path="/review">
                     <Review></Review>
                 </Route>
-                <Route path="/inventory">
+                <Route path="/orders">
                     <Inventory></Inventory>
                 </Route>
                 
